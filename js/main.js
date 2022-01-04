44590ca1fece6664cf76682daf5822c72c69ceb8
parkdl <ehddlf2395@gmail.com>
2022-01-04 17:47:22 +0900

    main content and control

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const deg = 45;
 let i = 0;
 
 for (let article of articles) {
+    let pic = article.querySelector('.pic');
     article.style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
-
+    pic.style.backgroundImage = `url(img/member${i + 1}.jpg)`;
     i++;
 }
